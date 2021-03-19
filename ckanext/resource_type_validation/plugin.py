4e2182a536e7e1792c27dc05a2aaf70e5854c936
antuarc <carl.antuar@smartservice.qld.gov.au>
2021-03-19 16:41:55 +1000

    [QOL-7849] drop runtime configuration of support contact for now

--- ckanext/resource_type_validation/plugin.py
+++ ckanext/resource_type_validation/plugin.py
@@ -10,25 +10,7 @@ class ResourceTypeValidationPlugin(plugins.SingletonPlugin):
     Filename, file contents, and specified format must resolve to
     compatible types.
     """
-    plugins.implements(plugins.IConfigurer, inherit=True)
     plugins.implements(plugins.IResourceController, inherit=True)
-
-    # IConfigurer
-
-    def update_config(self, config):
-        """ Add our templates directory so we can add a config field.
-        """
-        plugins.toolkit.add_template_directory(config, 'templates')
-
-    def update_config_schema(self, schema):
-        """ Add a field to the schema to specify the support contact
-        (usually an email address).
-        """
-        schema.update({
-            'ckanext.resource_validation.support_contact': [
-                plugins.toolkit.get_validator('ignore_missing')
-            ]
-        })
 
     # IResourceController
 
