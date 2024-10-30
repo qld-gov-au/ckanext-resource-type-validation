238d366773bbec2b291e406f60a273729bc3ed2a
ThrawnCA <shell_layer-github@yahoo.com.au>
2024-10-30 12:35:30 +1000

    [QOLDEV-985] convert scenario test to create dataset using standard function

--- bin/serve.sh
+++ bin/serve.sh
@@ -3,7 +3,7 @@ set -e
 
 . ${APP_DIR}/bin/activate
 if (which ckan > /dev/null); then
-    ckan -c ${CKAN_INI} run -r
+    ckan -c ${CKAN_INI} run --disable-reloader --threaded
 else
     paster serve ${CKAN_INI}
 fi
