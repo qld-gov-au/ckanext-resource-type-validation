475cd281fef6db4a16c7066791d5d82682901525
ThrawnCA <carl.antuar@smartservice.qld.gov.au>
2025-04-16 12:56:37 +1000

    handle CKAN 2.9 lacking Context type

--- ckanext/resource_type_validation/plugin.py
+++ ckanext/resource_type_validation/plugin.py
@@ -3,7 +3,6 @@
 from typing import Any
 
 from ckan import plugins
-from ckan.types import Context
 
 from .resource_type_validation import ResourceTypeValidator
 
@@ -26,20 +25,20 @@ class ResourceTypeValidationPlugin(plugins.SingletonPlugin):
     # IResourceController
 
     # CKAN 2.9
-    def before_create(self, context: Context, data_dict: 'dict[str, Any]'):
+    def before_create(self, context: Any, data_dict: 'dict[str, Any]'):
         self.before_resource_create(context, data_dict)
 
-    def before_update(self, context: Context, current: 'dict[str, Any]', data_dict: 'dict[str, Any]'):
+    def before_update(self, context: Any, current: 'dict[str, Any]', data_dict: 'dict[str, Any]'):
         self.before_resource_update(context, current, data_dict)
 
     # CKAN 2.10
-    def before_resource_create(self, context: Context, data_dict: 'dict[str, Any]'):
+    def before_resource_create(self, context: Any, data_dict: 'dict[str, Any]'):
         """ Check that uploads have an acceptable mime type.
         """
         assert self.validator
         self.validator.validate_resource_mimetype(data_dict)
 
-    def before_resource_update(self, context: Context, current: 'dict[str, Any]', data_dict: 'dict[str, Any]'):
+    def before_resource_update(self, context: Any, current: 'dict[str, Any]', data_dict: 'dict[str, Any]'):
         """ Check that uploads have an acceptable mime type.
         """
         assert self.validator
