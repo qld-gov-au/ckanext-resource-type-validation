d0f1bcb530ceed3a8af77e8022de3c504fb5205d
william dutton <will.dutt@gmail.com>
2025-04-16 11:00:31 +1000

    chore: uplift cicd

--- bin/process-artifacts.sh
+++ bin/process-artifacts.sh
@@ -8,6 +8,11 @@ set -e
 # avoid this script to fail when copying artifacts.
 ahoy cli "mkdir -p test/screenshots test/junit"
 
+ahoy cli "junit2html test/junit/results.xml test/junit/pytest-results.html"
+ahoy cli "coverage html -d test/junit/coverage_html"
+
+ahoy cli "coverage report xml -o test/junit/coverage.xml"
+
 # Copy from the app container to the build host for storage.
 mkdir -p /tmp/artifacts/behave /tmp/artifacts/junit
 docker cp "$(sh bin/docker-compose.sh ps -q ckan)":/srv/app/test/screenshots /tmp/artifacts/behave/
