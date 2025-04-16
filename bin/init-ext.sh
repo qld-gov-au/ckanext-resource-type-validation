716e0ae7e1e2c5898ec9b73139de92e0b40c77e1
william dutton <will.dutt@gmail.com>
2025-04-16 11:09:19 +1000

    chore: uplift cicd

--- bin/init-ext.sh
+++ bin/init-ext.sh
@@ -41,9 +41,9 @@ for extension in . `ls -d $SRC_DIR/ckanext-*`; do
     install_requirements $extension requirements pip-requirements
 done
 pip install -e .
-installed_name=$(grep '^\s*name=' setup.py |sed "s|[^']*'\([-a-zA-Z0-9]*\)'.*|\1|")
+installed_name=$(grep '^\s*name\s*=' pyproject.toml | sed 's|[^"]*"\([-a-zA-Z0-9]*\)".*|\1|')
 
 # Validate that the extension was installed correctly.
 if ! pip list | grep "$installed_name" > /dev/null; then echo "Unable to find the extension in the list"; exit 1; fi
 
-. "${APP_DIR}"/bin/deactivate
+. "${APP_DIR}"/bin/deactivate || echo "deactivate failure, continuing"
