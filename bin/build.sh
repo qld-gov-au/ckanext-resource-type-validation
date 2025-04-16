15bed7a0177997b53fc85cb5e96a896fc95072bf
ThrawnCA <carl.antuar@smartservice.qld.gov.au>
2025-04-16 14:50:58 +1000

    drop duplicate test summary action, make Solr version more robust
    - Second test summary run was incorrectly picking up the coverage report file and not knowing what to do with it.
    - Solr version can be determined by the build script, no need to customise in the workflow as we don't want to try testing on different versions for the same CKAN.

--- bin/build.sh
+++ bin/build.sh
@@ -17,9 +17,15 @@ ahoy pull
 PYTHON=python3
 PYTHON_VERSION=py3
 
+SOLR_VERSION=9
+if [ "$CKAN_VERSION" = "2.9" ]; then
+    SOLR_VERSION=8
+fi
+
 sed "s|{CKAN_VERSION}|$CKAN_VERSION|g" .docker/Dockerfile-template.ckan \
     | sed "s|{PYTHON_VERSION}|$PYTHON_VERSION|g" \
     | sed "s|{PYTHON}|$PYTHON|g" \
     > .docker/Dockerfile.ckan
 
+export SOLR_VERSION
 ahoy build
